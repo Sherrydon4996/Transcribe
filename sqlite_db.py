8479340e1a52e79edbb2e31e9a4c0b4d6a4bd6d6
Sherrydon4996 <156075110+Sherrydon4996@users.noreply.github.com>
2024-09-23 08:34:27 +0300

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -309,28 +309,18 @@ def save_user_comments(username, full_name, comment):
 
 
 
-# def add_to_user_comment(username, full_name, comment):
-#     connection = sqlite3.connect('transcribed_data.db')
-#     cursor = connection.cursor()
-#     cursor.execute("insert into user_comments(username, full_name, comment) values(?,?,?)",
-#                    (username, full_name, comment))
-#     connection.commit()
-#     connection.close()
-
-
-# add_to_user_comment("harryedu", "garrison edu", "Great work, keep it up")
-# add_to_user_comment("peterN", "peter ngacha", "This app is awsome, now i will be visiting your github often")
-
-def add_to_user_comment():
+def add_to_user_comment(username, full_name, comment):
     connection = sqlite3.connect('transcribed_data.db')
     cursor = connection.cursor()
-    cursor.execute("delete from user_comments"
-                )
+    cursor.execute("insert into user_comments(username, full_name, comment) values(?,?,?)",
+                   (username, full_name, comment))
     connection.commit()
     connection.close()
 
 
-add_to_user_comment()
+add_to_user_comment("harryedu", "Edwin", "Great work, keep it up bro.")
+add_to_user_comment("peterN", "peter ngacha", "This app is awsome, now i will be visiting your github often")
+
 
 
 
